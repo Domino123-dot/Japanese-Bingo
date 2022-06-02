e96a44941acbdc73e7a22da0de90716615bd6384
Domino123-dot <pl.kubakot@Gmail.com>
2022-06-02 02:32:53 +0200

    added content to footer component

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -8,19 +8,31 @@ const Footer = () => {
   return (
     <>
       <Line color="#241f1f" margin="20% 5% 0 5%" />
+      
       <div className={styles.footer}>
+      <div className={styles.text}>
+          My first project! If you have any questions contact me via e-mail.
+        </div>
         <div className={styles.icons}>
-          <Icon icon={faEnvelope}>pl.kubakot@Gmail.com</Icon>
+          <Icon link="mailto:pl.kubakot@Gmail.com" icon={faEnvelope}>
+            pl.kubakot@Gmail.com
+          </Icon>
         </div>
 
         <div className={styles.icons}>
-          <Icon icon={faGithub}>Github.com/Domino123-dot</Icon>
-        </div>
-        <div className={styles.icons} >
-          <Icon icon={faLinkedin}>
-            www.linkedin.com/in/jakub-kot-337154112/
+          <Icon link="https://github.com/Domino123-dot" icon={faGithub}>
+            My Github
           </Icon>
         </div>
+        <div className={styles.icons}>
+          <Icon
+            link="https://www.linkedin.com/in/jakub-kot-337154112/"
+            icon={faLinkedin}
+          >
+            LinkedIn
+          </Icon>
+        </div>
+        
       </div>
     </>
   );
--- components/icon/icon.js
+++ components/icon/icon.js
@@ -1,11 +1,11 @@
 import react from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../icon/icon.module.scss";
-const Icon = ({icon,children}) =>{
+const Icon = ({icon,children , link}) =>{
 return(
 <>
-<div>
-<FontAwesomeIcon className={styles.icon} icon={icon} /> {children} 
+<div >
+<a href={link} className={styles.a}><FontAwesomeIcon icon={icon} /> {children} </a>
 
 </div>
 
